53e9f76c748da914fbe7460a077387462306298b
ChaitanyaNawade <chaitanyanawde0@gmail.com>
2026-04-17 12:09:19 +0530

    code to sort an array in descending order using bubble sort

---
+++ DesSortBubble.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+class ArrayOperation
+{
+    public int Arr[];
+
+    public ArrayOperation(int Arr[])
+    {
+        this.Arr = Arr;
+    }
+
+    public void BubbleSort()
+    {
+        int i = 0 , j = 0, n = Arr.length,temp = 0;
+
+        for(i = 0 ; i < n ; i++)
+        {
+            for(j = 0 ; j < n - 1 - i; j++)
+            {
+                if(Arr[j] < Arr[j+1])
+                {
+                    temp = Arr[j];
+                    Arr[j] = Arr[j+1];
+                    Arr[j+1]  = temp;
+                }
+            }
+        }
+    }
+
+    public void Display()
+    {
+        int i = 0;
+
+        for(i = 0 ; i < Arr.length; i++)
+        {
+            System.out.print(Arr[i]+" ");
+        }
+    }
+}
+
+class DesSortBubble 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iSize = 0 , i = 0;
+
+        System.out.println("Enter the size of the array");
+
+        iSize = sobj.nextInt();
+
+        int Arr[] = new int[iSize];
+
+        System.out.println("Enter the elements in an array");
+
+        for(i = 0  ; i < Arr.length ;i++)
+        {
+            Arr[i] = sobj.nextInt();
+        }
+
+        ArrayOperation aobj = new ArrayOperation(Arr);
+
+        aobj.BubbleSort();
+
+        aobj.Display();
+
+        sobj.close();
+
+    }
+}
